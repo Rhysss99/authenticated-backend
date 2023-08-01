b855801f7b81a37fd5f2743a9a9a2248c1ec7729
RHYS.CHORLTON <Rhys.Chorlton@fdmgroup.com>
2023-08-01 16:05:04 +0100

    Added AuthenticationController

---
+++ src/main/java/com/fdmgroup/AuthenticatedBackend/controllers/AuthenticationController.java
@@ -0,0 +1,28 @@
+package com.fdmgroup.AuthenticatedBackend.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fdmgroup.AuthenticatedBackend.model.ApplicationUser;
+import com.fdmgroup.AuthenticatedBackend.model.RegistrationDTO;
+import com.fdmgroup.AuthenticatedBackend.services.AuthenticationService;
+
+@RestController
+@RequestMapping("/auth")
+@CrossOrigin("*")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping("/register")
+    public ApplicationUser registerUser(@RequestBody RegistrationDTO body){
+        return authenticationService.registerUser(body.getUsername(), body.getPassword());
+    }
+
+    
+}
