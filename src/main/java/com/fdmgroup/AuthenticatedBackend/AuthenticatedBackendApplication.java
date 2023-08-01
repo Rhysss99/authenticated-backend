3ae1c0a070c1ebeacc6e291636bf1da99fc71e05
RHYS.CHORLTON <Rhys.Chorlton@fdmgroup.com>
2023-08-01 12:20:21 +0100

    Ensure both the roles and an admin user is created on application start

--- src/main/java/com/fdmgroup/AuthenticatedBackend/AuthenticatedBackendApplication.java
+++ src/main/java/com/fdmgroup/AuthenticatedBackend/AuthenticatedBackendApplication.java
@@ -1,13 +1,38 @@
 package com.fdmgroup.AuthenticatedBackend;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import java.util.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import com.fdmgroup.AuthenticatedBackend.model.ApplicationUser;
+import com.fdmgroup.AuthenticatedBackend.model.Role;
+import com.fdmgroup.AuthenticatedBackend.repository.RoleRepository;
+import com.fdmgroup.AuthenticatedBackend.repository.UserRepository;
 
 @SpringBootApplication
 public class AuthenticatedBackendApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AuthenticatedBackendApplication.class, args);
+	}
+
+	@Bean
+	CommandLineRunner run(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder){
+		return args ->{
+			if(roleRepository.findByAuthority("ADMIN").isPresent()) return;
+			Role adminRole = roleRepository.save(new Role("ADMIN"));
+			roleRepository.save(new Role("USER"));
+
+			Set<Role> roles = new HashSet<>();
+			roles.add(adminRole);
+
+			ApplicationUser admin = new ApplicationUser(1, "admin", passwordEncoder.encode("password"), roles);
+
+			userRepository.save(admin);
+		};
 	}
 
 }
